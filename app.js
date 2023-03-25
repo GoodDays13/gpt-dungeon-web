fc1e44b7be920efaf0bb818cfd1824967977b5cd
Good_Days13 <kaiden.matuskiewicz@gmail.com>
2023-03-25 19:46:30 -0400

    Moves js out of html

---
+++ app.js
@@ -0,0 +1,48 @@
+// Initialize the history variable with some sample messages
+let history = [
+    "Hello, how can I assist you?",
+    "What's your name?",
+    "AI assistant."
+  ];
+
+const messageForm = document.getElementById("message-form");
+const messageInput = document.getElementById("message");
+const messagesDiv = document.getElementById("messages");
+
+function displayMessage(message) {
+messagesDiv.insertAdjacentHTML("beforeend", `<p>${message}</p>`);
+}
+
+function displayHistory() {
+messagesDiv.innerHTML = "";
+history.forEach(displayMessage);
+}
+
+displayHistory();
+
+function handleFormSubmit(event) {
+event.preventDefault();
+const message = messageInput.value.trim();
+if (message) {
+    history.push(message);
+    displayMessage(`You inputted: ${message}`);
+    messageInput.value = "";
+    messagesDiv.scrollTop = messagesDiv.scrollHeight;
+    
+    // Send input data to server and handle response
+    fetch('http://localhost:5000/getResponse', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ input: message })
+    })
+    .then(response => response.json())
+    .then(data => {
+    const response = data.response;
+    history.push(response);
+    displayMessage(response);
+    })
+    .catch(error => console.error(error));
+}
+}
+
+messageForm.addEventListener("submit", handleFormSubmit);
