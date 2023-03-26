5cc0e6099ca66000ba01b6a74063357c4e7db137
Good_Days13 <kaiden.matuskiewicz@gmail.com>
2023-03-26 17:55:44 -0400

    Revert "New attempt to fix keyboard on chrome"
    This reverts commit fcb2503a123073d05d872e4b16c3d8c706171a4b.

--- app.js
+++ app.js
@@ -5,21 +5,21 @@ const messageForm = document.getElementById("message-form");
 const messageInput = document.getElementById("message");
 const messagesDiv = document.getElementById("messages");
 
-// // Set the height of the messages container
-// function setMessagesHeight() {
-//   const height = window.innerHeight - messageForm.offsetHeight;
-//   messagesDiv.style.height = `${height}px`;
-// }
+// Set the height of the messages container
+function setMessagesHeight() {
+  const height = window.innerHeight - messageForm.offsetHeight;
+  messagesDiv.style.height = `${height}px`;
+}
 
-// // Call setMessagesHeight on load and resize
-// window.addEventListener('DOMContentLoaded', setMessagesHeight);
-// window.addEventListener('resize', setMessagesHeight);
+// Call setMessagesHeight on load and resize
+window.addEventListener('DOMContentLoaded', setMessagesHeight);
+window.addEventListener('resize', setMessagesHeight);
 
-// // Submit the form
-// messageForm.addEventListener('submit', (event) => {
-//   event.preventDefault();
-//   // your code to handle the form submission goes here
-// });
+// Submit the form
+messageForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  // your code to handle the form submission goes here
+});
 
 
 function colorQuotes(message) {
